f36ac3b37ec68aaffc09cd93dd6b35e2626893fd
Nicolas Joyard <joyard.nicolas@gmail.com>
2009-12-27 09:31:42 +0100

    Updated object interface

--- pydomserver/helpers/Amule.py
+++ pydomserver/helpers/Amule.py
@@ -20,232 +20,328 @@ import time
 from amule import AmuleClient, ECConnectionError
 
 from ..RunWatcherThread import RunWatcherThread
-from ..Objects import ObjectProvider
+from ..Objects import ObjectProvider, ObjectProcessor
 from ..SocketInterface import SIPacket, SIStringTag, SIUInt32Tag, SIUInt8Tag
 from ..SocketInterfaceCodes import SIC
 
 
 WATCHER_INITIAL_WAIT = 2
 
-               
-class AmuleRunWatcherThread(RunWatcherThread):
 
-    def __init__(self, domserver, logger, command, **kwargs):
-        RunWatcherThread.__init__(self, domserver, logger, command, **kwargs)
-        self.ec_connected = False
-        self.ec_client = AmuleClient()
-        self.last_search = ''
+class DictDownload:
+
+    def __init__(self, domserver, amule, hash):
+        self.domserver = domserver
+        self.am = amule
+        self.hash = hash
         
-    def _connect(self):
+    def pause(self):
         try:
-            self.ec_client.connect(
+            return self.am.client.partfile_pause([self.hash])
+        except ValueError:
+            return False
+    
+    def resume(self):
+        try:
+            return self.am.client.partfile_resume([self.hash])
+        except ValueError:
+            return False
+            
+    def cancel(self):
+        try:
+            return self.am.client.partfile_delete([self.hash])
+        except ValueError:
+            return False
+        
+    def __getitem__(self, key):
+        self.am._update()
+        if key in ('name', 'size', 'speed'):
+            return self.am.downloads[self.hash][key]
+        elif key == 'status':
+            stopped = self.am.downloads[self.hash].get('stopped', False)
+            status = self.am.downloads[self.hash]['status']
+            size = self.am.downloads[self.hash]['size']
+            done = self.am.downloads[self.hash]['size_done']
+            seeds_xfer = self.am.downloads[self.hash]['src_xfer']
+            if stopped:
+                return 0
+            elif status == 7:
+                return 2
+            elif seeds_xfer > 0:
+                return 3
+            elif size == done:
+                return 5
+            else:
+                return 1
+        elif key == 'seeds':
+            return self.am.downloads[self.hash]['src_count']
+        elif key == 'progress':
+            size = self.am.downloads[self.hash]['size']
+            done = self.am.downloads[self.hash]['size_done']
+            return done / size * 100.0 if size > 0 else 0
+        elif key == 'hash':
+            return self.hash
+        else:
+            raise KeyError("DictDownload has no item named '%s'" % key)
+            
+            
+class DictResult:
+
+    def __init__(self, domserver, amule, hash):
+        self.domserver = domserver
+        self.am = amule
+        self.hash = hash
+        
+    def download(self):
+        try:
+            # TODO manage last_added
+            return self.am.client.download_search_results([self.hash])
+        except ValueError:
+            return False
+        
+    def __getitem__(self, key):
+        self.am._update()
+        if key in ('name', 'size'):
+            return self.am.results[self.hash][key]
+        elif key == 'seeds':
+            return self.am.results[self.hash]['src_count']
+        elif key == 'downloading':
+            return self.am.results[self.hash]['src_count_xfer']
+        elif key == 'hash':
+            return self.hash
+        else:
+            raise KeyError("DictResult has no item named '%s'" % key)
+            
+            
+class Amule:
+
+    def __init__(self, domserver, logger):
+        self.domserver = domserver
+        self.log = logger
+        self.connected = False
+        self.client = AmuleClient()
+        self.objs = None
+        self.last_updated = 0
+        self.downloads = {}
+        self.results = {}
+        
+    def register_object_provider(self, objs):
+        self.objs = objs
+    
+    def connect(self):
+        try:
+            self.client.connect(
                 'localhost',
                 int(self.domserver.config['amule.ec_port']),
                 self.domserver.config['amule.ec_password'],
                 'domserver',
                 'TODOversion'
             )
-        except ECConnectionError:
-            self.info("RunWatcher: could not connect to amule")
-            self.ec_connected = False
+        except ECConnectionError, e:
+            self.log.info("Could not connect to amule - <%s>" % e)
+            self.connected = False
         else:
-            self.info("RunWatcher: connected to amule")
-            self.ec_connected = True
+            self.log.info("Connected to amule")
+            self.connected = True
+            
+    def disconnect(self):
+        if self.connected:
+            self.objs.save_on_stop()
+            self.client.disconnect()
+            
+    def __getitem__(self, key):
+        kind, hash = key.split("/", 1)
+        if kind == 'download':
+            return DictDownload(self.domserver, self, hash)
+        elif kind == 'result':
+            return DictResult(self.domserver, self, hash)
+            
+    def _update(self):
+        interval = int(self.domserver.config['amule.update_interval'])
+        if self.last_updated + interval < time.time():
+            self.domserver.debug("dbg:: amule._update refetch")
+            self.downloads = self.client.get_download_list()
+            self.results = self.client.get_search_results()
+            self.last_updated = time.time()
+            
+    def keys(self):
+        if self.connected:
+            self._update()
+            d = ["download/%s" % h for h in self.downloads.keys()]
+            r = ["result/%s" % h for h in self.results.keys()]
+            return d + r
+        else:
+            return []
         
-    def on_start(self):
-        self.verbose("RunWatcher: waiting %d seconds before connecting..." % WATCHER_INITIAL_WAIT)
-        time.sleep(WATCHER_INITIAL_WAIT)
-        self._connect()
-            
-    def on_kill(self):
-        if self.ec_connected:
-            self.info("RunWatcher: disconnecting from amule")
-            self.ec_connected = False
-            self.ec_client.disconnect()
-            
-    def on_check(self):
-        if not self.ec_connected:
-            self.info("RunWatcher: retrying connection to amule...")
-            self._connect()
-            
-            
+        
 class AmuleObjectProvider(ObjectProvider):
     """Object provider for amule. Provides access to downloads and search
     results with two kinds of objects: 'amule:download/<hash>' and
     'amule:result/<hash>'.
     """
-    
-    downloads = {}
-    downloads_last_update = 0
-    results = {}
-    results_last_update = 0
 
-    def __init__(self, domserver, helper):
+    def __init__(self, domserver, am):
         ObjectProvider.__init__(self, domserver, 'amule')
-        self.helper = helper
-        
-    def _amclient(self):
-        return self.helper.get_amule_client()
-        
-    def _update_downloads(self):
-        interval = self.domserver.config['amule.update_interval']
-        if self.downloads_last_update + interval > time.time():
-            return
-        client = self._amclient()
-        if client:
-            queue = client.get_download_list()
-            for hash in queue.keys():
-                stopped = queue[hash].get('stopped', False)
-                status = queue[hash]['status']
-                size = queue[hash]['size']
-                done = queue[hash]['size_done']
-                seeds_xfer = queue[hash]['src_xfer']
-                speed = queue[hash]['speed']
+        self.am = am
+        self.am.register_object_provider(self)
                 
-                # domserver dl statuses:
-                #   0   stop
-                #   1   init
-                #   2   pause
-                #   3   dl
-                #   4   ul
-                #   5   finalize
-                #   6   done
-                
-                if stopped:
-                    stnum = 0
-                elif status == 7:
-                    stnum = 2
-                elif seeds_xfer > 0:
-                    stnum = 3
-                elif size == done:
-                    stnum = 5
-                else:
-                    stnum = 1
-                
-                self.downloads[hash] = {
-                    'name': queue[hash]['name'],
-                    'size': size,
-                    'seeds': queue[hash]['src_count'],
-                    'speed': queue[hash]['speed'],
-                    'progress': done/size*100 if size != 0 else 0,
-                    'status': stnum
+    def save_on_stop(self):
+        for oid in self.am.keys():
+            if oid.startswith('download/'):
+                tdata = {
+                    "name": self.am[oid]["name"],
+                    "size": self.am[oid]["size"],
+                    "seeds": 0,
+                    "progress": self.am[oid]["progress"],
+                    "speed": 0,
+                    "status": 0
                 }
-        self.downloads_last_update = time.time()
-        
-    def _update_results(self):
-        interval = self.domserver.config['amule.update_interval']
-        if self.results_last_update + interval > time.time():
-            return
-        client = self._amclient()
-        if client:
-            res = client.get_search_results()
-            for hash in res.keys():
-                self.results[hash] = {
-                    'name': res[hash]['name'],
-                    'size': res[hash]['size'],
-                    'seeds': res[hash]['src_count'],
-                    'downloading': res[hash]['src_count_xfer']
-                }
-        self.results_last_update = time.time()
-        
-    def load_downloads(self):
-        for oid in self.list_objects():
-            kind, hash = oid.split('/', 1)
-            if kind == 'download':
-                self.downloads[hash] = self.load_object(hash)
-    
-    def save_downloads(self):
-        for hash in self.downloads.keys():
-            # TODO use actual amule statuses
-            if self.downloads[hash]['status'] < 6:
-                self.downloads[hash]['status'] = 0
-            self.save_object("download/%s" % hash, self.torrents[hash])
+                self.save_object(oid, tdata)
         
     def get_oids(self):
-        self._update_downloads()
-        self._update_results()
-        doids = [ "download/%s" % k for k in self.downloads.keys() ]
-        roids = [ "result/%s" % k for k in self.results.keys() ]
-        return doids + roids
+        return list(set(self.am.keys()) | set(self.list_objects()))
         
-    def valid_oid(self, oid):
-        try:
-            kind, hash = oid.split('/', 1)
-        except ValueError:
-            return False
-            
-        if kind == 'download':
-            self._update_downloads()
-            return hash in self.downloads.keys()
-        elif kind == 'result':
-            self._update_results()
-            return hash in self.results.keys()
-        else:
-            return False
+    def valid_oid(self, hash):
+        return hash in self.get_oids()
             
     def get_types(self, oid):
         try:
             kind, hash = oid.split('/', 1)
         except ValueError:
-            return
-        
-        if kind in ('download', 'result'):
-            return [kind, 'virtual-file']
+            raise ObjectError('invalid-oid:%s' % oid)
+
+        if kind == 'download':
+            return ['download', 'amule-partfile']
+        elif kind == 'result':
+            return ['result', 'amule-result']
             
     def get_value(self, oid, prop):
+        obj_exists = False
+        if oid in self.am.keys():
+            obj_exists = True
+            try:
+                return self.am[oid][prop]
+            except KeyError:
+                pass
+        
+        try:
+            return self.load_object_property(oid, prop)
+        except ObjectError:
+            if obj_exists:
+                raise KeyError("No property '%s' for '%s'" % (prop, oid))
+            else:
+                raise
+            
+    def set_value(self, oid, prop, val):
+        if prop not in ('date_started', 'hash') or not oid.startswith('download/'):
+            raise KeyError("Property '%s' is readonly" % prop)
+        self.save_object_property(oid, prop, val)
+        
+    def describe_props(self, oid, detail_level):
         kind, hash = oid.split('/', 1)
         
+        desc = {}
+        for k in ('name', 'hash'):
+            desc[k] = {'type':'string'}
+        desc['size'] = {
+            'type':'uint32',
+            'conv':(lambda x: int(x / 1024))
+        }
+        
         if kind == 'download':
-            self._update_downloads()
-            return self.downloads[hash][prop]
+            for k in ('speed', 'seeds', 'status', 'date_started'):
+                desc[k] = {'type':'uint32'}
+            desc['progress'] = {
+                'type':'string',
+                'conv':(lambda x: "%.2f%%" % x)
+            }
         elif kind == 'result':
-            self._update_results()
-            return self.results[hash][prop]
+            desc['seeds'] = {'type':'uint32'}
             
-    def to_sitags_download(self, hash, detail_level):
-        tags = []
-        dl = self.downloads[hash]
-        keys = set(['name', 'size', 'seeds', 'speed', 'progress',
-                    'status']) & set(dl.keys())
-        for k in keys:
-            if k in ('size',):
-                vtag = SIUInt32Tag(int(dl[k] / 1024), SIC.TAG_OBJ_VALUE)
-            elif k in ('speed', 'seeds'):
-                vtag = SIUInt32Tag(dl[k], SIC.TAG_OBJ_VALUE)
-            else:
-                vtag = SIStringTag(dl[k], SIC.TAG_OBJ_VALUE)
+        return desc
+        
+        
+class AmuleObjectProcessor(ObjectProcessor):
 
-            tag = SIStringTag(k, SIC.TAG_OBJ_PROPERTY)
-            tag.subtags.append(vtag)
-            tags.append(tag)
-        return tags
+    def __init__(self, domserver, name, objs):
+        ObjectProcessor.__init__(self, domserver, name)
+        self.objs = objs
         
-    def to_sitags_result(self, hash, detail_level):
-        tags = []
-        keys = set('seeds', 'seeds_xfer', 'name', 'size')
-        res = self.results[hash]
-        for k in set(res.keys) & keys:
-            if k in ('size',):
-                vtag = SIUInt32Tag(int(res[k] / 1024), SIC.TAG_OBJ_VALUE)
-            elif k in ('seeds', 'seeds_xfer'):
-                vtag = SIUInt32Tag(res[k], SIC.TAG_OBJ_VALUE)
-            else:
-                vtag = SISTringTag(res[k], SIC.TAG_OBJ_VALUE)
-            tag = SIStringTag(k, SIC.TAG_OBJ_PROPERTY)
-            tag.subtags.append(vtag)
-            tags.append(tag)
-        return tags
+    def get_action_names(self, obj=None):
+        names = []
+        if obj and obj.is_a("amule-partfile"):
+            status = obj["status"]
+            if status < 4:
+                names.append('partfile-cancel')
+            if status > 0 and status < 5:
+                names.append('partfile-pause' if status != 2 else 'partfile-resume')
+            if status == 6:
+                names.append('partfile-clear')
+        if obj and obj.is_a('amule-result'):
+            if obj['downloading'] < 1:
+                names.append('result-download')
+        if not obj:
+            names.append('amule-search')
+        return names
+        
+    def describe_action(self, act):
+        name = act.name
+        obj = act.obj
+        
+        if name == 'amule-search':
+            act.add_param('query', SIC.APFLAG_TYPE_STRING)
+            act.add_param('search-type', SIC.APFLAG_TYPE_NUMBER)
+            act.add_param('file-type', SIC.APFLAG_TYPE_STRING)
+            act.add_param('min-size', SIC.APFLAG_TYPE_NUMBER | SIC.APFLAG_OPTION_OPTIONAL)
+            act.add_param('max-size', SIC.APFLAG_TYPE_NUMBER | SIC.APFLAG_OPTION_OPTIONAL)
+            act.add_param('avail', SIC.APFLAG_TYPE_NUMBER | SIC.APFLAG_OPTION_OPTIONAL)
+            act.add_param('file-ext', SIC.APFLAG_TYPE_STRING | SIC.APFLAG_OPTION_OPTIONAL)
+        
+    def execute_action(self, act):
+        name = act.name
+        obj = act.obj
+        
+        if name.startswith("partfile-"):
+            pf = self.objs.am["download/%s" % obj['hash']]
+            if name == 'partfile-cancel':
+                pf.cancel()
+            elif name == 'partfile-pause':
+                pf.pause()
+            elif name == 'partfile-resume':
+                pf.resume()
+            elif name == 'partfile-clear':
+                # TODO
+                pass
+        elif name == 'result-download':
+            rs = self.objs.am["result/%s" % obj['hash']]
+            rs.download()
+        elif name == 'amule-search':
+            ac = self.objs.am.client
+            ac.search_start(act['query'], act['search-type'], act['min-size'],
+                act['max-size'], act['file-type'], act['avail'], act['file-ext'])
+        
+        
+class AmuleRunWatcherThread(RunWatcherThread):
+
+    def __init__(self, am, domserver, logger, command, **kwargs):
+        RunWatcherThread.__init__(self, domserver, logger, command, **kwargs)
+        self.am = am
+        
+    def on_start(self):
+        self.verbose("RunWatcher: waiting %d seconds before connecting..." % WATCHER_INITIAL_WAIT)
+        time.sleep(WATCHER_INITIAL_WAIT)
+        self.am.connect()
             
-    def to_sitags(self, oid, detail_level):
-        kind, hash = oid.split('/', 1)
-        
-        if kind == 'download':
-            self._update_downloads()
-            return self.to_sitags_download(hash, detail_level)
-        elif kind == 'result':
-            self._update_results()
-            return self.to_sitags_result(hash, detail_level)
-        
+    def on_kill(self):
+        if self.am.connected:
+            self.info("RunWatcher: disconnecting from amule")
+            self.am.disconnect()
+            
+    def on_check(self):
+        if not self.am.connected:
+            self.info("RunWatcher: retrying connection to amule...")
+            self.am.connect()
+    
     
 class AmuleHelper:
 
@@ -255,10 +351,15 @@ class AmuleHelper:
         self.domserver.info("Initializing amule helper")
         self.logger = domserver.get_logger('amule.log_file', 'amule.log_level')
         
-        self.objs = AmuleObjectProvider(domserver, self)
+        self.amule = Amule(self.domserver, self.logger)
+        
+        self.objs = AmuleObjectProvider(domserver, self.amule)
+        self.proc = AmuleObjectProcessor(domserver, 'amule', self.objs)
+        
         domserver.register_object_interface(
             name='amule',
-            provider=self.objs
+            provider=self.objs,
+            processor=self.proc
         )
         
         self.update_amule_config()
@@ -279,6 +380,7 @@ class AmuleHelper:
         
     def enable(self):
         self._rw_thread = AmuleRunWatcherThread(
+            self.amule,
             self.domserver,
             self.logger,
             '/usr/share/amule/amuled_home_wrapper.sh',
