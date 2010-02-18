68c4ad01fbeb84fcdcb3eced45f3cf50367e4d7e
Nicolas JOYARD <niko@nestor.(none)>
2010-02-18 20:02:04 +0100

    File manager: updated object interface; storage devices now detected using DeviceKit-disks instead of (deprecated) hal

--- pynestor/helpers/filemanager.py
+++ pynestor/helpers/filemanager.py
@@ -14,98 +14,218 @@
 # along with nestor.  If not, see <http://www.gnu.org/licenses/>.
 
 import dbus
-from dbus.mainloop.glib import DBusGMainLoop, threads_init
+import dbus.mainloop.glib
 import gobject
 import os
 import os.path
+import subprocess
 
-from ..objects import OCriterion, OExpression, ObjectProvider, ObjectProcessor
-from ..socketinterface import SIStringTag, SIUInt32Tag
+import pynestor.objects as o
 from ..socketinterfacecodes import SIC
 from ..thread import Thread
 
 class StorageDeviceWatcher(Thread):
+
+    DKPATH = "/org/freedesktop/DeviceKit/Disks"
+    DKDOMAIN = "org.freedesktop.DeviceKit.Disks"
 
     def __init__(self, nestor, objs, logger=None):
         Thread.__init__(self, nestor, logger)
         self.objs = objs
         
         gobject.threads_init()
-        threads_init()
-        DBusGMainLoop(set_as_default=True)
+        dbus.mainloop.glib.threads_init()
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
         self.loop = gobject.MainLoop()
         
         self.bus = dbus.SystemBus()
-        self.hal_manager_obj = self.bus.get_object(
-            "org.freedesktop.Hal", 
-            "/org/freedesktop/Hal/Manager"
-        )
-        self.hal_manager = dbus.Interface(
-            self.hal_manager_obj,
-            "org.freedesktop.Hal.Manager"
-        )
-        self.hal_manager.connect_to_signal("DeviceAdded", self._filter_add)
+        self.dkproxy = self.bus.get_object(self.DKDOMAIN, self.DKPATH)
+        self.dkiface = dbus.Interface(self.dkproxy, self.DKDOMAIN)
+        self.dkiface.connect_to_signal("DeviceAdded", self._device_added)
+        self.dkiface.connect_to_signal("DeviceChanged", self._device_changed)
+        self.dkiface.connect_to_signal("DeviceRemoved", self._device_removed)
+            
+    def _device_added(self, device):
+        self.debug("Device %s added" % device)
         
-    def _filter_add(self, udi):
-        device_obj = self.bus.get_object ("org.freedesktop.Hal", udi)
-        device = dbus.Interface(device_obj, "org.freedesktop.Hal.Device")
-        if device.QueryCapability("volume"):
-            return self._device_added(device)
-            
-    def _device_added(self, volume):
-        device_file = volume.GetProperty("block.device")
-        label = volume.GetProperty("volume.label")
-        fstype = volume.GetProperty("volume.fstype")
-        mounted = volume.GetProperty("volume.is_mounted")
-        mount_point = volume.GetProperty("volume.mount_point")
-        try:
-            size = volume.GetProperty("volume.size")
-        except:
-            size = 0
-            
-        self.verbose("New storage device: %s ('%s', %s, %.2fGB)" %
-            (device_file, label, fstype, float(size) / 1024**3))
-        self.objs.add_device(device_file, label, fstype, size, mounted,
-            mount_point)
+    def _device_changed(self, device):
+        self.debug("Device %s changed" % device)
+        self.objs.device_changed(str(device))
+        
+    def _device_removed(self, device):
+        self.debug("Device %s removed" % device)
+        self.objs.device_changed(str(device))
             
     def nestor_run(self):
-        self.verbose("Starting storage device watcher thread...")
+        self.verbose("Starting disk watcher thread...")
         self.loop.run()
-        self.verbose("Stopped storage device watcher thread")
+        self.verbose("Stopped disk watcher thread")
         
     def stop(self):
         self.loop.quit()
         
+    def get_device_list(self):
+        """Return an array of disk DBus paths sorted by mountpoint, with USB
+        drives last"""
         
-class FileObjectProvider(ObjectProvider):
+        devices = []
+        domain = "%s.Device" % self.DKDOMAIN
+        for d in self.dkiface.EnumerateDevices():
+            dpath = str(d)
+            device = self.bus.get_object(self.DKDOMAIN, dpath) 
+            diface = dbus.Interface(device, "org.freedesktop.DBus.Properties")
+            if str(diface.Get(domain, "id-usage")) == 'filesystem':
+                mounted = bool(diface.Get(domain, "device-is-mounted"))
+                mpaths = [str(p) for p in diface.Get(domain, "device-mount-paths")]
+                mpath = mpaths[0] if mpaths else ''
+                is_usb = str(diface.Get(domain, "drive-connection-interface")) == 'usb'
+                devices.append({
+                    'path': dpath,
+                    'mounted': mounted,
+                    'mpath': mpath,
+                    'is_usb': is_usb
+                })
+                
+        devices.sort(key=lambda x:x['mpath'])
+        devices.sort(key=lambda x:x['is_usb'])
+        return [d['path'] for d in devices]
+        
+    def get_device_props(self, dpath):
+        """Return a dict of properties for device with DBus path 'dpath'"""
+        
+        device = self.bus.get_object(self.DKDOMAIN, dpath)
+        diface = dbus.Interface(device, "org.freedesktop.DBus.Properties")
+        domain = "%s.Device" % self.DKDOMAIN
+        
+        mounted = bool(diface.Get(domain, "device-is-mounted"))
+        mpaths = [str(p) for p in diface.Get(domain, "device-mount-paths")]
+        mpath = mpaths[0] if mpaths else ''
+        size = int(diface.Get(domain, "device-size"))
+        devfile = str(diface.Get(domain, "device-file"))
+        uuid = str(diface.Get(domain, "id-uuid"))
+        is_usb = str(diface.Get(domain, "drive-connection-interface")) == 'usb'
+        fstype = str(diface.Get(domain, "id-type"))
+        
+        idlabel = str(diface.Get(domain, "id-label"))
+        drivemodel = str(diface.Get(domain, "drive-model"))
+        presname = str(diface.Get(domain, "device-presentation-name"))
+        
+        for lbl in (presname, idlabel, mpath, drivemodel, devfile):
+            if lbl != '':
+                label = lbl
+                break
+        
+        return {
+            "dbus-path": dpath,
+            "mounted": mounted,
+            "mount-point": mpath,
+            "name": label,
+            "size": size,
+            "free": size,
+            "devfile": devfile,
+            "uuid": uuid,
+            "is_usb": is_usb,
+            "fstype": fstype
+        }
+        
+        
+class FileObject(o.ObjectWrapper):
+
+    def describe(self):
+        if os.path.isdir(self.oid):
+            self.types = ["directory"]
+        else:
+            self.types = ["file"]
+        
+        statinfo = os.stat(self.oid)
+        self.props = {
+            'path': self.oid,
+            'basename': os.path.basename(self.oid),
+            'dirname': os.path.dirname(self, oid),
+            'size': statinfo.st_size,
+            'owner': statinfo.st_uid,
+            'group': statinfo.st_gid,
+            'perms': statinfo.st_mode,
+            'mtime': statinfo.st_mtime
+        }
+    
+    def update(self):
+        pass
+        
+    def set_value(self, key, value):
+        raise KeyError("FileObject.%s is read-only" % key)
+        
+        
+class DeviceObject(o.ObjectWrapper):
+
+    def describe(self):
+        self.types = ["device"]
+        path = self.oid[len('#'):]
+        self.props = self.provider.dev.get_device_props(path)
+        self.props["uuid"] = self.props["uuid"].translate(None, "-_:")
+        self.update()
+        
+    def update(self):
+        if self.props["mounted"]:
+            out = subprocess.Popen(
+                ['df', '-B', '1', self.props['mount-point']],
+                stdout=subprocess.PIPE
+            ).communicate()[0].split('\n')[1:]
+            line = out[0].split()
+            try:
+                self.props['size'] = int(line[1])
+                self.props['free'] = int(line[3])
+            except IndexError:
+                # When device name is too long, df outputs on 2 lines
+                line = out[1].split()
+                self.props['size'] = int(line[0])
+                self.props['free'] = int(line[2])
+        
+    def set_value(self, key, value):
+        raise KeyError("DeviceObject.%s is read-only" % key)
+        
+        
+class FileObjectProvider(o.ObjectProvider):
     """Filesystem object provider
     
     Provides files and folders as file:/path/to/item as well as external storage
-    devices as file:#/dev/xxx"""  
+    devices as file:#<DBus DeviceKit-disk path>"""  
 
     _fprops = ['size', 'path', 'basename', 'dirname', 'owner', 'group', 'perms', 'mtime']
     _dprops = ['size', 'path', 'label', 'fs', 'dev']
     devices = {}
     
-    def add_device(self, dev, label, fs, size, mounted, mntpoint):
-        self.devices[dev]= {
-            'label': label, 'fs': fs, 'size': size, 'mounted': mounted,
-            'mntpoint': mntpoint
-        }
+    def set_devthread(self, devthread):
+        self.dev = devthread
         
-    def del_device(self, dev):
-        del self.devices[dev]
+    def device_changed(self, devpath):
+        self.cache.remove("file:#%s" % devpath)
     
     def get_oids(self):
+        """Only return devices, files/directories must be matched"""
+        return ["#%s" % path for path in self.dev.get_device_list()]
+        
+    def get_object(self, oid):
+        if oid.startswith("#"):
+            if oid[len("#"):] in self.dev.get_device_list():
+                return DeviceObject(self.nestor, self, oid)
+        elif oid.startswith("/") and os.path.exists(oid):
+            return FileObject(self.nestor, self, oid)
+        
+    def infer_oids(self, obj):
+        try:
+            path = obj["path"]
+        except KeyError:
+            pass
+        else:
+            if os.path.exists(path):
+                return [path]
         return []
-        
-    def matching_oids(self, expr, types):
-        ret = []
-        
-        if not types or 'device' in types:
-            ret.extend(["#%s" % d for d in self.devices.keys()])
     
-        if not types or 'file' in types or 'folder' in types:
+    def match_oids(self, expr, types):
+        ret = []
+    
+        if not types or 'file' in types or 'directory' in types:
             # Only handle expressions with one criterion on dirname
             if not (expr.oper != '' or
                 expr.crit_a is None or
@@ -130,125 +250,39 @@ class FileObjectProvider(ObjectProvider):
                         objs.extend(f)
                     d[0:len(d)] = []
                 
-                ret.extend([os.path.join(dirname, o) for o in objs])
+                ret.extend([os.path.join(dirname, obj) for obj in objs])
+                
+        if not types or 'device' in types:
+            ret.extend(o.ObjectProvider.match_oids(self, expr, types))
             
-        return list(set(ret))
-        
-    def valid_oid(self, path):
-        if path.startswith('#'):
-            return path[1:] in self.devices.keys()
-        elif path.startswith('/'):
-            return os.path.exists(path)
-        else:
-            return False
-        
-    def get_types(self, path):
-        types = []
-        if path.startswith('#'):
-            types.append('device')
-            if self.devices[path[1:]][mounted]:
-                types.append('folder')
-        elif path.startswith('/'):
-            if os.path.isdir(path):
-                types.append('folder')
-            elif os.path.isfile(path):
-                types.append('file')
-        return types
-        
-    def get_value(self, path, prop):
-        if path.startswith('#'):
-            if prop not in self._dprops:
-                raise KeyError("No property '%s' for object '%s'" % (prop, path))
-            dev = self.devices[path[1:]]
-            if prop == 'path':
-                if dev['mounted']:
-                    return dev['mntpoint']
-                else:
-                    return ''
-            elif prop == 'dev':
-                return path[1:]
-            else:
-                return dev[prop]
-        elif path.startswith('/'):
-            if prop not in self._fprops:
-                raise KeyError("No property '%s' for object '%s'" % (prop, path))
-                
-            if prop in ['size', 'owner', 'group', 'perms', 'mtime']:
-                statinfo = os.stat(path)
-                if prop == 'size':
-                    return statinfo.st_size
-                elif prop == 'owner':
-                    return statinfo.st_uid
-                elif prop == 'group':
-                    return statinfo.st_gid
-                elif prop == 'perms':
-                    return statinfo.st_mode
-                elif prop == 'mtime':
-                    return statinfo.st_mtime
-            elif prop == 'basename':
-                return os.path.basename(path)
-            elif prop == 'dirname':
-                return os.path.dirname(path)
-            elif prop == 'path':
-                return path
-                
-    def set_value(self, path, prop, val):
-        raise KeyError("Property '%s' is readonly" % prop)
-        
-    def describe_props(self, path, lod):
-        props = []
-        if path.startswith('#'):
-            if lod >= SIC.LOD_BASIC:
-                props.extend(['label'])
-            if lod == SIC.LOD_MAX:
-                props.extend(['size', 'path', 'fs', 'dev'])
-                
-        elif path.startswith('/'):
-            if lod >= SIC.LOD_BASIC:
-                props.extend(['path'])
-            if lod == SIC.LOD_MAX:
-                props.extend(['size', 'owner', 'group', 'mtime', 'perms',
-                    'basename', 'dirname'])
-                    
-        desc = {}
-        for k in props:
-            if k == 'size':
-                if path.startswith('#'):
-                    desc[k] = {
-                        'type': 'uint32',
-                        'conv': lambda x:int(x / 1024 ** 2)
-                    }
-                elif path.startswith('/'):
-                    desc[k] = {
-                        'type': 'uint32',
-                        'conv': lambda x:int(x / 1024)
-                    }
-            elif k in ('path', 'label', 'fs', 'dev', 'basename', 'dirname'):
-                desc[k] = {'type': 'string'}
-            elif k in ('owner', 'group', 'mtime', 'perms'):
-                desc[k] = {'type': 'uint32'}
-        return desc
+        return ret        
         
         
-class FileObjectProcessor(ObjectProcessor):
+class FileObjectProcessor(o.ObjectProcessor):
 
-    def get_action_names(self, obj=None):
+    def get_action(self, obj):
         names = []
-        if obj is not None and obj.is_oneof(['file', 'folder']):
+        if obj.is_oneof(['file', 'directory']):
             path = obj['path']
             dirname = os.path.dirname(path)
             if os.access(dirname, os.W_OK) and dirname != path:
-                names.extend(['delete', 'rename'])
+                names.extend(['file-delete', 'file-rename'])
+        if obj.is_a('device'):
+            names.append('device-set-label')
+            if obj["mounted"]:
+                names.append('device-unmount')
+            else:
+                names.append('device-mount')
         return names
         
-    def describe_action(self, act):
+    def describe(self, act):
         name = act.name
         obj = act.obj
             
         if name == 'rename':
             act.add_param('new-name', SIC.APFLAG_TYPE_STRING)
             
-    def execute_action(self, act):
+    def execute(self, act):
         name = act.name
         obj = act.obj
             
@@ -272,3 +306,10 @@ class FileManagerHelper:
             provider=self.objs,
             processor=self.proc
         )
+        
+        self.dwthread = StorageDeviceWatcher(nestor, self.objs, self.logger)
+        self.dw_tid = nestor.add_thread(self.dwthread, True)
+        
+        self.objs.set_devthread(self.dwthread)
+        
+        
