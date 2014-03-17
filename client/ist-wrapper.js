17fcbd4e3588c817cd6767513bc44c82662e3c2e
Nicolas Joyard <joyard.nicolas@gmail.com>
2014-03-17 09:31:07 +0100

    Update to new ist.js directive interface

--- client/ist-wrapper.js
+++ client/ist-wrapper.js
@@ -5,87 +5,85 @@ define(["ist", "ajax", "dom", "login"], function(ist, ajax, dom, login) {
 	"use strict";
 
 
-	ist.helper("if-right", function(context, value, tmpl, fragment) {
-		var rendered = fragment.extractRenderedFragment();
-	
-		if (login.hasRight(value)) {
+	ist.helper("if-right", function(context, value, tmpl, iterate) {
+		iterate(login.hasRight(value) ? [value] : [], function(key, rendered) {
 			if (rendered) {
 				rendered.update(context);
 			} else {
-				rendered = tmpl.render(context);
+				return tmpl.render(context);
 			}
-
-			fragment.appendRenderedFragment(rendered);
-		}
-	});
-
-
-	ist.helper("svg", function(context, value, tmpl, fragment) {
-		var data = typeof value === "string" ? { src: value } : value,
-			container = fragment.firstChild;
-
-		if (!container) {
-			container = context.createElement("span");
-			container.classList.add("svg-container");
-			fragment.appendChild(container);
-		}
-
-		if (container.loading === data.src || container.loaded === data.src) {
-			// Already loading or loaded this SVG, do nothing
-			return;
-		}
-
-		container.loading = data.src;
-		delete container.loaded;
-
-		ajax.cachedXML(data.src).then(function(xml) {
-			var svg = context.importNode(xml.querySelector("svg"), true);
-
-			if (container.loading !== data.src) {
-				// Already loading an other URI for this SVG, ignore this one
-				return;
-			}
-
-			delete container.loading;
-			container.loaded = data.src;
-
-			// Allow resize
-			svg.setAttribute("preserveAspectRatio", "xMinYMin meet");
-			svg.setAttribute("viewBox", "0 0 " + svg.getAttribute("width") + " " + svg.getAttribute("height"));
-
-			// Set misc attributes
-			if (data.title) {
-				svg.setAttribute("title", data.title);
-			}
-
-			if (container.firstChild) {
-				container.replaceChild(svg, container.firstChild);
-			} else {
-				container.appendChild(svg);
-			}
-		}).otherwise(function(err) {
-			if (container.loading !== data.src) {
-				// Already loading an other URI for this SVG, ignore the error
-				return;
-			}
-
-			delete container.loading;
-			console.error(err);
 		});
 	});
 
 
-	ist.helper("behave", function(context, value, tmpl, fragment) {
-		var rendered = fragment.extractRenderedFragment();
+	ist.helper("svg", function(context, value, tmpl, iterate) {
+		iterate(function(key, rendered) {
+			var data = typeof value === "string" ? { src: value } : value;
 
-		if (rendered) {
-			rendered.update(context.value);
-		} else {
-			rendered = tmpl.render(context.value);
-		}
+			if (!rendered) {
+				rendered  = context.createElement("span");
+				rendered.classList.add("svg-container");
+			}
 
-		fragment.appendRenderedFragment(rendered);
-		dom.behave(fragment, value);
+			if (rendered.loading === data.src || rendered.loaded === data.src) {
+				// Already loading or loaded this SVG, do nothing
+				return;
+			}
+
+			rendered.loading = data.src;
+			delete rendered.loaded;
+
+			ajax.cachedXML(data.src).then(function(xml) {
+				var svg = context.importNode(xml.querySelector("svg"), true);
+
+				if (rendered.loading !== data.src) {
+					// Already loading an other URI for this SVG, ignore this one
+					return;
+				}
+
+				delete rendered.loading;
+				rendered.loaded = data.src;
+
+				// Allow resize
+				svg.setAttribute("preserveAspectRatio", "xMinYMin meet");
+				svg.setAttribute("viewBox", "0 0 " + svg.getAttribute("width") + " " + svg.getAttribute("height"));
+
+				// Set misc attributes
+				if (data.title) {
+					svg.setAttribute("title", data.title);
+				}
+
+				if (rendered.firstChild) {
+					rendered.replaceChild(svg, rendered.firstChild);
+				} else {
+					rendered.appendChild(svg);
+				}
+			}).otherwise(function(err) {
+				if (rendered.loading !== data.src) {
+					// Already loading an other URI for this SVG, ignore the error
+					return;
+				}
+
+				delete rendered.loading;
+				console.error(err);
+			});
+
+			return rendered;
+		});
+	});
+
+
+	ist.helper("behave", function(context, value, tmpl, iterate) {
+		iterate(function(key, rendered) {
+			if (rendered) {
+				rendered.update(context.value);
+			} else {
+				rendered = tmpl.render(context.value);
+			}
+		
+			dom.behave(rendered, value);
+			return rendered;
+		});
 	});
 
 
