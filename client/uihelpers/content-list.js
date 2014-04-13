5de54bbe7bf94fe1ed69fcae8927611a9e194d39
Nicolas Joyard <joyard.nicolas@gmail.com>
2014-04-13 11:52:23 +0200

    Revert content list fetch fix, was not broken

--- client/uihelpers/content-list.js
+++ client/uihelpers/content-list.js
@@ -128,13 +128,13 @@ define(["router", "ui", "dom"], function(router, ui, dom) {
 					fetching = true;
 					view.loading.dispatch(true);
 
-					(fetcher ? fetcher() : watcher.fetch(fetchCount))
+					(fetcher ? fetcher(fetchCount) : watcher.fetch(fetchCount))
 					.then(function(docs) {
 						renderDocs(docs);
 						fetching = false;
 						view.loading.dispatch(false);
 
-						if (!fetcher && view.isEndVisible() && docs.length > 0) {
+						if (view.isEndVisible() && docs.length > 0) {
 							fetch();
 						}
 					})
@@ -193,7 +193,7 @@ define(["router", "ui", "dom"], function(router, ui, dom) {
 					watcher = null;
 				});
 			} else {
-				if (!fetcher && view.isEndVisible()) {
+				if (view.isEndVisible()) {
 					fetch();
 				}
 			}
