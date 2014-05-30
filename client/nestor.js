5e65c762ea0c49a7bbdeed526aaa5c1bab04796f
Nicolas Joyard <joyard.nicolas@gmail.com>
2014-05-30 19:30:39 +0200

    Restore cast_sender.js from google CDN

--- client/nestor.js
+++ client/nestor.js
@@ -27,8 +27,8 @@
 		},
 
 		paths: {
-			//"chromecast": "//www.gstatic.com/cv/js/sender/v1/cast_sender",
-			"chromecast": "/static/__cast_sender",
+			"chromecast": "//www.gstatic.com/cv/js/sender/v1/cast_sender",
+			//"chromecast": "/static/__cast_sender",
 			"socketio": "/socket.io/socket.io"
 		}
 	});
